da3bce9d5e3063690cad3e8e2314b272c04bbe7e
Ali Kartal <ali_kartal91@hotmail.com>
2021-12-12 21:18:49 +0300

    10daysofjavascript.js changed (day 1)

--- js/10daysofjavascript.js
+++ js/10daysofjavascript.js
@@ -1,4 +1,7 @@
-/******** Day 0: Hello, World! ********/
+ // 10 Days of JavaScript
+
+
+/***    Day 0: Hello, World! ***/
 
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
@@ -9,4 +12,39 @@ function greeting(parameterVariable) {
 }
 
 
+
+/***   Day 1: Arithmetic Operators ***/
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width
+    
+    return area;
+}
+
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter= 2 * (length+width)
+
+    return perimeter;
+}
 
