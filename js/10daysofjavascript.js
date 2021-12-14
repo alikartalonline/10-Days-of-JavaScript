83954f59a3d68bf5d84267c4116401b6a6ab822c
Ali Kartal <ali_kartal91@hotmail.com>
2021-12-15 01:14:41 +0300

    10daysofjavascript.js changed

--- js/10daysofjavascript.js
+++ js/10daysofjavascript.js
@@ -82,3 +82,26 @@ function getPerimeter(length, width) {
     return perimeter;
 }
 
+
+
+/***   Day 1: Let and Const ***/
+
+
+function main() {
+    
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+     
+    const PI = Math.PI;
+    let input = Number(readLine());
+    
+     // Print the area of the circle:
+    
+     console.log(PI* Math.pow(input,2));
+    
+    // Print the perimeter of the circle:
+    
+    console.log(2 * PI * input); 
+
+    }
+
+
