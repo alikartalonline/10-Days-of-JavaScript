c8c7c656b1c0af076d9b03319e021aff506175fb
Ali Kartal <ali_kartal91@hotmail.com>
2022-06-17 17:25:59 +0300

    "Day 7: Regular Expressions III" added

--- js/10daysofjavascript.js
+++ js/10daysofjavascript.js
@@ -712,6 +712,22 @@ console.log(regexVar("Mr.X"))
 
 
 
+//*************  Day 7: Regular Expressions III  *************
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    const re = new RegExp(/[0-9]+/gm);
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
 
 
 
